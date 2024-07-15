44b67fed9e4110b7af63671ec6fed08310321159
ry1674 <riku.yamauchi0813@gmaol.com>
2024-07-15 12:40:24 +0900

    commit_kadai_18

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,11 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	public void setGivenName() {
+		givenName = "花子";
+	}
+	
+	public void eachIntoroduce() {
+		System.out.println("趣味は読書です");
+	}
+}
